67b59916369ac6bdc21fa106e8d378b53b4caf46
Rishad <50762398+rishadblack@users.noreply.github.com>
2023-10-01 17:38:35 +0600

    fix setup file

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import loginCommand from "./commands/login.mjs";
 program.version("1.0.0").description("My CLI Tool");
 
 // Register your commands
-program.addCommand(loginCommand);
-program.addCommand(downloadCommand);
+program.addCommand(loginCommand, { name: "login" });
+program.addCommand(downloadCommand, { name: "pull" });
 program.addCommand(watchCommand, { name: "watch" });
 
 program.parse(process.argv);
